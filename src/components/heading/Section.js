0d382dbbbf4fbe0a86a0055ad079f10b623a709b
Andrew <addineen@gmail.com>
2023-03-18 09:14:52 -0800

    Updated intro wording

--- src/components/heading/Section.js
+++ src/components/heading/Section.js
@@ -8,7 +8,7 @@ const Section = () => {
 	const Intro = {
 		subTitle: "/ This is me",
 		title: "Aspiring Software Engineer.",
-		paragraph: "I'm a consultant seeking employment as a backend software engineer. When I'm not programming, I like to read and study interesting academic subjects.",
+		paragraph: "I'm a consultant seeking employment as a software engineer. When I'm not programming, I like to read and study interesting academic subjects.",
 		link: "My story"
 	}
 	return (<div className="primary-background heading-section">
